1adf5382da22d0db1256b804e33ac932f2a1e35e
Bartek <grzebyk44@gmail.com>
2017-09-15 15:45:22 +0200

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,30 @@
     </div>
     <div class="welcome-explore text-center">
     	<h2>Poznaj Alibre Design</h2>
+    	<div class="welcome-explore-item col-md-4 col-sm-6 col-xs-12">
+    		<div class="welcome-explore-image"></div>
+    		<div class="welcome-explore-title"><a href="#">Modelowanie 3D</a></div>
+    	</div>
+    	<div class="welcome-explore-item col-md-4 col-sm-6 col-xs-12">
+    		<div class="welcome-explore-image"><img src="/images/icons/dokumentacja-2d.jpg"></div>
+    		<div class="welcome-explore-title"><a href="#">Dokumentacja 2D</a></div>
+    	</div>
+    	<div class="welcome-explore-item col-md-4 col-sm-6 col-xs-12">
+    		<div class="welcome-explore-image"><img src="/images/icons/modul-blach.jpg"></div>
+    		<div class="welcome-explore-title"><a href="#">Moduł blach</a></div>
+    	</div>
+    	<div class="welcome-explore-item col-md-4 col-sm-6 col-xs-12">
+    		<div class="welcome-explore-image"><img src="/images/icons/fotorealistyczny-rendering.jpg"></div>
+    		<div class="welcome-explore-title"><a href="#">Fotorealistyczny rendering</a></div>
+    	</div>
+    	<div class="welcome-explore-item col-md-4 col-sm-6 col-xs-12">
+ 			<div class="welcome-explore-image"></div>
+ 			<div class="welcome-explore-title"><a href="#">Chmura punktów</div></a> 
+    	</div>
+    	<div class="welcome-explore-item col-md-4 col-sm-6 col-xs-12">
+    		<div class="welcome-explore-image"><img src="/images/icons/wymiana-danych-cad.jpg"></div>
+    		<div class="welcome-explore-title"><a href="#">Wymiana danych CAD</div></a>
+    	</div>
     </div>
     <div class="welcome-different">
     	<h2 class="text-center">Co nas wyróżnia</h2>
